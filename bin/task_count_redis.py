696b1d237bbd287b68dd4b325eeefa843f347021
Hao <haowang@ece.utoronto.ca>
2017-03-21 21:52:13 -0400

    update script

--- bin/task_count_redis.py
+++ bin/task_count_redis.py
@@ -5,15 +5,15 @@ import time
 import datetime
 import redis
 
-from config import Config
-from resp_parse import *
-
 sys.path.insert(0, '../sparkvent')
+from sparkvent.config import Config
+from sparkvent.resp_parse import *
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__ + "/../"))
 
 
 def main():
-    config = Config(os.path.abspath(ROOT_DIR + "/conf/config.yml.template"))
+    config = Config(os.path.abspath(ROOT_DIR + "/conf/config.yml"))
     parser = ParserFactory.get_parser(config.type, config.server)
     redis_host, redis_port = config.redis.split(":")
     db = redis.Redis(host=redis_host, port=redis_port)
